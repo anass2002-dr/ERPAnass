94d36839f146825d15141e3e02f84da2eef91b7c
anass2002-dr <anassdermaj163@gmail.com>
2024-08-30 21:54:57 +0100

    30_08_2024_21_54

--- ERP_Anass_app/src/app/pages/purchases/Purchases/list_Purchases/list_Purchases.component.ts
+++ ERP_Anass_app/src/app/pages/purchases/Purchases/list_Purchases/list_Purchases.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { each } from 'jquery';
 import { filter } from 'rxjs';
+import { Article } from 'src/app/models/Article/Article';
+import { ProductService } from 'src/app/Services/Articles/product.service';
 import { erp_anass } from 'src/main';
 
 @Component({
@@ -11,13 +17,37 @@ import { erp_anass } from 'src/main';
 })
 export class List_PurchasesComponent implements OnInit {
 
+  displayedColumns: string[] = ['idArticle', 'articleName', 'familyName', 'stockQuantity', 'purchasePrice', 'sellingPrice', 'update', 'delete'];
+  dataSource = new MatTableDataSource();
+  list: Article[] = [];
+  loading: boolean = true;
   breadcrumbs: any[] = [];
-  constructor(private route: ActivatedRoute) { }
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _liveAnnouncer: LiveAnnouncer, private router: Router, private productService: ProductService, private route: ActivatedRoute) { }
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
   ngOnInit(): void {
     this.breadcrumbs = erp_anass.title_header(this.route)
-
   }
 
+  delete() {
+    console.log();
 
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
