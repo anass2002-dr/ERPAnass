3992ef4d7b6cbdc7bfde2f14eba1b36032a78608
anass2002-dr <anassdermaj163@gmail.com>
2024-09-01 19:32:39 +0100

    01_09_2024_19_32

--- ERP_Anass_app/src/app/app.component.ts
+++ ERP_Anass_app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component ,ElementRef, NgModule} from '@angular/core';
+import { Component, ElementRef, NgModule } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -7,12 +7,15 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'admindashboard';
-  constructor(private elementRef: ElementRef,  public  _router: Router) { }
+  constructor(private elementRef: ElementRef, public _router: Router) { }
 
   ngOnInit() {
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.src = "../assets/js/main.js";
     this.elementRef.nativeElement.appendChild(s);
+  }
+  hide() {
+
   }
 }
